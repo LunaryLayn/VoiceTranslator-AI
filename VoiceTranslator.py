7bd81786004e0f80721ea8a33c14f8b6d0220918
LunaryLayn <hupogo2000@protonmail.com>
2023-04-03 23:55:19 +0200

    dist added

--- VoiceTranslator.py
+++ VoiceTranslator.py
@@ -3,7 +3,7 @@ import Transcriber
 import Translator
 import VoiceSpeaker
 
-
+print("Bienvenido a VoiceTranslator: Recuerda abrir VoiceVox en Google Collab para obtener la URL: https://colab.research.google.com/drive/1nwWSLQN7_dPS5Dq4EAXg3hLrkfrDFbLi#scrollTo=TlYOWAolCjq-")
 URL = input("URL de VoiceVox: ")
 
 Recorder.record()
